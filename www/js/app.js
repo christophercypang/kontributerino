623aff589f9c0c93d450c126005f99ed1fdffbe9
imranhabib <imranhabib14@hotmail.com>
2015-11-15 01:23:04 -0800

    new home page

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('kontribute', ['ionic', 'kontribute.controllers', 'kontribute.services', 'kontribute.factories','firebase','ui.bootstrap', 'ui.bootstrap.datetimepicker'])
+angular.module('kontribute', ['ionic', 'kontribute.controllers', 'kontribute.services', 'kontribute.factories','firebase'])
 
 
 .config(function($ionicConfigProvider) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -230,3 +230,32 @@ function(){
 })
 
 
+.controller('MapController', function($scope, $ionicLoading) {
+ 
+    $scope.initialise = function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    };
+    google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise())
+
+ 
+});
+
